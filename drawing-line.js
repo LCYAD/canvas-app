a8261fd49fd408fe5b916098b3e96fd43312e75f
shawn137080 <shawn137080@gmail.com>
2017-10-30 17:41:58 +0800

    color picker

--- drawing-line.js
+++ drawing-line.js
@@ -5,7 +5,7 @@ class DrawingLine extends PaintFunction{
     }
     
     onMouseDown(coord,event){
-        this.context.strokeStyle = "#df4b26";
+        this.context.strokeStyle = rgbaColor;
         this.context.lineJoin = "round";
         this.context.lineWidth = 5;
         this.context.beginPath();
--- drawing-rectangle.js
+++ drawing-rectangle.js
@@ -6,12 +6,12 @@ class DrawingRectangle extends PaintFunction{
     }
     
     onMouseDown(coord,event){
-        this.contextReal.fillStyle = "#f44";
+        this.contextReal.fillStyle = rgbaColor;
         this.origX = coord[0];
         this.origY = coord[1];
     }
     onDragging(coord,event){
-        this.contextDraft.fillStyle = "#f44";
+        this.contextDraft.fillStyle = rgbaColor;
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
         this.contextDraft.fillRect(this.origX,this.origY,coord[0]- this.origX,coord[1] - this.origY)
     }
